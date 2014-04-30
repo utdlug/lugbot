60b90f11a4a325aca866c47a1f387a6ceba52f82
aaron-lebo <aaron.m.lebo@gmail.com>
2014-04-30 14:48:41 -0500

    Update lugbot.js

--- lugbot.js
+++ lugbot.js
@@ -2,8 +2,15 @@ var irc = require('irc');
 
 var bot = new irc.Client('irc.oftc.net', 'lug-bot', {channels: ['#utdlug']});
 bot.addListener('join', function(channel, who) {
-  if (who == 'xy86') {
-    bot.send('MODE', '#utdlug +o ' + who);
-    bot.say('#utdlug', 'such lug');
+  switch (who) {
+    case 'cyanode':
+    case 'desmond':
+    case 'phy1729':
+    case 'theplague':
+    case 'xy86':
+      bot.send('MODE', who, '+o');
+      break;
+    default:
+      bot.say('#utdlug', 'hi ' + who);
   };
 });
