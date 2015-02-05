f4262701bc1543cfc4cd3b82032b413347062904
Aaaron Lebo <aaron.matthew.lebo@mgial.com>
2015-02-05 15:18:24 -0600

    formatting

--- lugbot.js
+++ lugbot.js
@@ -8,20 +8,20 @@ var CHAN = '#utdlug';
 var bot = new irc.Client('irc.oftc.net', 'utdlug', {channels: [CHAN]});
 var pinboard = new Pinboard(process.argv[2]);
 
-bot.addListener('message', function (from, to, msg) {
-  twitter.extractUrls(msg).map(function (url) {
+bot.addListener('message', function(from, to, msg) {
+  twitter.extractUrls(msg).map(function(url) {
     try {
       if (url.substring(0, 4) != 'http') {
         url = 'http://' + url;
       };
     
-      request(url, function (err, res, body) {
+      request(url, function(err, res, body) {
         pinboard.add({
           url: url, 
           description: cheerio.load(body)('title').text() || url,
           tags: ['by:' + from].concat(twitter.extractHashtags(msg)).join(','),
           replace: 'no',
-        }, function (res) {
+        }, function(res) {
           console.log(res)
         })
       });
