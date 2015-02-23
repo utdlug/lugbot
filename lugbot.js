6d1dae3830352d894a245424e2db826a608c0e68
Aaaron Lebo <aaron.matthew.lebo@mgial.com>
2015-02-23 06:23:23 -0600

    welcome visitors

--- lugbot.js
+++ lugbot.js
@@ -8,6 +8,33 @@ var CHAN = '#utdlug' + (process.argv[3] ? 'testing' : '');
 var bot = new irc.Client('irc.oftc.net', 'utdlug', {channels: [CHAN]});
 var pinboard = new Pinboard(process.argv[2]);
 
+bot.addListener('join', function(chan, nick) {
+    if (['argonaut',
+         'cyanode',
+         'drijen',
+         'Freek',
+         'Imster',
+         'jtm',
+         'justanull',
+         'kennyloglins',
+         'kennyloggins',
+         'm_wynn',
+         'MajObviousman',
+         'maldridge',
+         'maldridge-weechat',
+         'ninjabox',
+         'phy1729',
+         'programisto',
+         'scrollback1',
+         'SuperNoeMan',
+         'tig3rp4w',
+         'travnewmatic',
+         'utdlug'
+	].indexOf(nick) == -1) {
+        bot.say(CHAN, 'Hey, ' + nick + '. Good to see you here. If you have a question or want to talk about something, stick around. You might not get an immediate response, but someone will get back with you; people check this channel off and on day and night. Keep on luggin.');
+    } 
+});
+
 bot.addListener('message', function(from, to, msg) {
   twitter.extractUrls(msg).map(function(url) {
     try {
